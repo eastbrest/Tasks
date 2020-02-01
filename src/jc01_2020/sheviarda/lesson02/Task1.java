fcffa0229a700dac4702d1fe655488f01d256ac6
eastbrest <eastbrest@yahoo.com>
2020-02-01 23:30:24 +0100

    testy etih zadanij projdeny uspeshno

--- src/jc01_2020/sheviarda/lesson02/Task1.java
+++ src/jc01_2020/sheviarda/lesson02/Task1.java
@@ -19,5 +19,18 @@ public class Task1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int numeric = scanner.nextInt();
+		switch (numeric) {
+			case 1:
+				System.out.println("Число 1");
+				break;
+			case 2:
+				System.out.println("Число 2");
+				break;
+			case 3:
+				System.out.println("Число 3");
+				break;
+			default:
+				System.out.println("Другое число");
+		}
 	}
 }
